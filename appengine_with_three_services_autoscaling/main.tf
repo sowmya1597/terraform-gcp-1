b056981376c4bbf158206f6f9af376c37332e8af
Veena Venkata Sai Sowmya Samavedam <saisowmya.svv@cloudexpertsguru.com>
2023-07-02 17:17:40 +0530

    dbservice updated

--- appengine_with_three_services_autoscaling/main.tf
+++ appengine_with_three_services_autoscaling/main.tf
@@ -18,7 +18,7 @@ module "appengine_app" {
 }
 
 module "app_service" {
-  source            = "../modules/appservice/"
+  source            = "../modules/appservice"
   service_version   = var.app_service_version
   service           = var.app_service
   runtime           = var.app_service_runtime
@@ -31,8 +31,8 @@ module "app_service" {
 }
 
 module "web_service" {
-  source            = "../modules/webservice/"
- service_version    = var.web_service_version
+  source            = "../modules/webservice"
+  service_version    = var.web_service_version
   service           = var.web_service
   runtime           = var.web_service_runtime
   threadsafe        = var.web_service_threadsafe
@@ -43,15 +43,99 @@ module "web_service" {
   automatic_scaling = var.automatic_scaling
 }
 
-module "db_service" {
-  source            = "../modules/dbservice/"
-  service_version   = var.db_service_version
-  service           = var.db_service
-  runtime           = var.db_service_runtime
-  threadsafe        = var.db_service_threadsafe
-  instance_class    = var.db_service_instance_class
-  zip               = var.db_service_zip
-  handlers          = var.db_service_handlers
-  instances         = var.db_service_instances
+# ==== VPC network definition ==== #
+
+module "vpc_network" {
+  source           = "./../modules/vpc/vpc_network"
+  vpc_network_name = var.vpc_network_name
+  vpc_description  = "VPC network created to house the CSQL instance private IP."
+  routing_mode     = var.routing_mode
+}
+
+# ==== Allocated IP range definition ==== #
+
+module "allocated_ip_address_range" {
+  source                    = "../modules/vpc/allocated_ip_address_range"
+  name                      = var.allocated_ip_address_range_name
+  description               = "Allocation for the Cloud SQL instance."
+  prefix_length             = var.prefix_length
+  address_type              = "INTERNAL"
+  purpose                   = "VPC_PEERING"
+  associated_vpc_network_id = module.vpc_network.vpc_network_id
+
+}
+
+module "private_connection" {
+  source                      = "../modules/vpc/private_connection"
+  associated_vpc_network_id   = module.vpc_network.vpc_network_id
+  allocated_ip_address_ranges = [module.allocated_ip_address_range.name]
+}
+
+module "private_sqlserver_instance" {
+
+  # This is needed as we need to first peer with the service producer before assigning a private IP address with Cloud SQL.
+  depends_on = [module.private_connection]
+
+  source = "../modules/vpc/cloud_sql_sqlserver"
+  name             = var.instance_name
+  database_version = var.sqlserver_version
+  cloud_sql_region = var.sql_region
+  # We are disabling the Public IP from the Cloud SQL instance as
+  # we only want to access it through private IP address.
+  ipv4_enabled   = false
+  vpc_network_id = module.vpc_network.vpc_network_id
+
+}
+
+# Second-generation instances include a default 'root'@'%' user with no password. 
+#This user will be deleted by Terraform on instance creation. 
+# Therefore, we will create a google_sql_user to define a custom user with a restricted host and strong password.
+
+module "cloud_sql_user" {
+  source = "../modules/vpc/cloud_sql_user"
+
+  sql_user_name           = var.sql_user_name
+  cloud_sql_instance_name = module.private_sqlserver_instance.name
+  sql_user_password       = var.sql_user_password
+}
+
+# ==== Cloud SQL Database ==== #
+
+module "cloud_sql_database" {
+  depends_on    = [module.private_sqlserver_instance]
+  source        = "../modules/vpc/cloud_sql_database"
+  database_name = var.database_name
+  instance_name = module.private_postgres_instance.name
+}
+
+# ==== Serverless VPC connector ==== #
+
+module "svpc_connector" {
+  depends_on     = [module.private_connection]
+  source         = "../modules/vpc/serverless_vpc_connector"
+  name           = var.svpc_connector_name
+  network        = var.vpc_network_name
+  ip_cidr_range  = var.ip_cidr_range
+  region         = var.region
+  min_throughput = var.min_throughput
+  max_throughput = var.max_throughput
+}
+
+# ==== App Engine Standard Automatic Scaling ==== #
+
+module "appengine_standard_automatic_scaling" {
+  depends_on        = [module.private_sqlserver_instance, module.svpc_connector, module.cloud_sql_database]
+  source            = "../modules/compute/standard/automatic_scaling/"
+  service_version   = var.service_version
+  service           = var.service
+  runtime           = var.runtime
+  threadsafe        = var.threadsafe
+  env_variables     = { CLOUD_SQL_CONNECTION_NAME : module.private_sqlserver_instance.connection_name, DB_USER : var.sql_user_name, DB_PASS : var.sql_user_password, DB_NAME : var.database_name }
+  instance_class    = var.instance_class
+  zip               = var.zip
+  entrypoint        = var.entrypoint
   automatic_scaling = var.automatic_scaling
+  vpc_access_connector = {
+    name = module.svpc_connector.id
+  }
 }
--- appengine_with_three_services_autoscaling/terraform.tfvars
+++ appengine_with_three_services_autoscaling/terraform.tfvars
@@ -86,3 +86,63 @@ automatic_scaling = {
     max_instances                 = 3
   }
 }
+
+
+### Variables definition for VPC network
+vpc_network_name = "private-csql-vpc-sqlserver"
+routing_mode     = "GLOBAL"
+
+### Variables definition for IP address allocation range
+allocated_ip_address_range_name = "private-ip-allocation-sqlserver"
+prefix_length                   = 16
+
+### Variables definition for Cloud SQL instance
+instance_name    = "private-sqlserver-instance-real"
+sql_region       = "europe-west1"
+sqlserver_version = "SQLSERVER_2019_STANDARD"
+
+## Variables definition for Cloud SQL user
+sql_user_name = "cloud-sql-sqlserver-user"
+
+## Variables declaration for Cloud SQL Database
+database_name = "terraform-db"
+
+# Variables for SVPC connector
+svpc_connector_name = "sqlserver-svpc-connector"
+ip_cidr_range       = "10.0.8.0/28"
+region              = "us-central1"
+min_throughput      = 200
+max_throughput      = 300
+
+# Variables for Standard Module
+service_version = "py1234"
+service         = "terraform-py37"
+runtime         = "python37"
+threadsafe      = true
+instance_class  = "F1"
+
+# Variables for Zip Module
+zip = {
+  source_url  = "https://storage.googleapis.com/$MY_BUCKET/gae-postgres-py27.zip"
+  files_count = null
+}
+
+# Variables for Entrypoint block
+entrypoint = {
+  shell = "python main.py"
+}
+
+# Variables for Automatic Scaling module
+automatic_scaling = {
+  max_concurrent_requests = 10,
+  max_idle_instances      = 10,
+  max_pending_latency     = "1s",
+  min_idle_instances      = 3,
+  min_pending_latency     = "0.01s",
+  standard_scheduler_settings = {
+    target_cpu_utilization        = 0.6,
+    target_throughput_utilization = 0.6,
+    min_instances                 = 1,
+    max_instances                 = 3
+  }
+}
