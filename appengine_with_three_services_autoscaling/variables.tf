610bd9b2b15006058ef8c6853b12c9ac341e092e
Veena Venkata Sai Sowmya Samavedam <saisowmya.svv@cloudexpertsguru.com>
2023-07-02 12:57:30 +0530

    second commit

--- appengine_with_three_services_autoscaling/variables.tf
+++ appengine_with_three_services_autoscaling/variables.tf
@@ -3,7 +3,7 @@ variable "location_id" {}
 variable "project" {}
 
 # ===== Variables for App Engine AppService  ===== #
-variable "service_version_v1" {}
+variable "app_service_version_v1" {}
 variable "runtime_v1" {}
 variable "threadsafe_v1" {}
 variable "instance_class_v1" {}
@@ -14,7 +14,7 @@ variable "instances_v1" {}
 
 
 # ===== Variables for App Engine WebService ===== #
-variable "service_version_v2" {}
+variable "web_service_version_v2" {}
 variable "runtime_v2" {}
 variable "threadsafe_v2" {}
 variable "instance_class_v2" {}
@@ -24,11 +24,11 @@ variable "instances_v2" {}
 # ===== /Variables for App Engine WebService ===== #
 
 # ===== Variables for App Engine DBService ===== #
-variable "service_version_v2" {}
-variable "runtime_v2" {}
-variable "threadsafe_v2" {}
-variable "instance_class_v2" {}
-variable "zip_v2" {}
-variable "handlers_v2" {}
-variable "instances_v2" {}
+variable "db_service_version_v3" {}
+variable "runtime_v3" {}
+variable "threadsafe_v3" {}
+variable "instance_class_v3" {}
+variable "zip_v3" {}
+variable "handlers_v3" {}
+variable "instances_v3" {}
 # ===== /Variables for App Engine DBService ===== #
